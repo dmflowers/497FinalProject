70f09d6432b720178d66ff87b0639edeb9499a12
dmflowers <44877686+dmflowers@users.noreply.github.com>
2018-12-02 14:45:40 -0600

    Updated registration screen

--- FinalProject_V2/Controllers/AccountController.cs
+++ FinalProject_V2/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using FinalProject_V2.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace FinalProject_V2.Controllers
 {
@@ -155,6 +156,12 @@ namespace FinalProject_V2.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                    await roleManager.CreateAsync(new IdentityRole("CanAddUsers"));
+                    await UserManager.AddToRoleAsync(user.Id, "CanAddUsers");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
